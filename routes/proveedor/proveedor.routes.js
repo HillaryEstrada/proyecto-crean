42b7c70d1979dcc2c0c8260bf07a68adc703ab53
HillaryEstrada <hillaryolivarria24.com>
2026-04-21 07:59:38 -0700

    ajuste en la routes proveedor

--- routes/proveedor/proveedor.routes.js
+++ routes/proveedor/proveedor.routes.js
@@ -4,14 +4,14 @@ const controller = require('../../controllers/proveedor/proveedor.controller');
 const { verificarToken, verificarModulo } = require('../../middleware/auth.middleware');
 
 router.use(verificarToken);
-router.use(verificarModulo('proveedor/proveedor'));
+// ← router.use(verificarModulo('proveedor/proveedor'));  QUITADA
 
 router.get('/', controller.listar);
 router.get('/todos', controller.listarTodos);
 router.get('/:id', controller.obtenerPorId);
-router.post('/', controller.crear);
-router.put('/:id', controller.actualizar);
-router.patch('/:id/desactivar', controller.desactivar);
-router.patch('/:id/reactivar', controller.reactivar);
+router.post('/', verificarModulo('proveedor/proveedor'), controller.crear);
+router.put('/:id', verificarModulo('proveedor/proveedor'), controller.actualizar);
+router.patch('/:id/desactivar', verificarModulo('proveedor/proveedor'), controller.desactivar);
+router.patch('/:id/reactivar', verificarModulo('proveedor/proveedor'), controller.reactivar);
 
 module.exports = router;
